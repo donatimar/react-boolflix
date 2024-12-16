6dbc150be9bdf7823ba213d96e5353e1afe17e58
donatimar <donatimar@gmail.com>
2024-12-16 16:30:44 +0100

    feat: create searchbar and button search

--- src/App.jsx
+++ src/App.jsx
@@ -7,7 +7,26 @@ function App() {
   const [selectedGenre, setSelectedGenre] = useState(null);
   const [seSelectedGenre, setSeSelectedGenre] = useState(null);
 
-  return <div></div>;
+  return (
+    <div className="container">
+      <h1 className="text-center mb-3">Movie</h1>
+
+      <div className="row justify-content-center mb-3">
+        <div className="col">
+          <div className="input-group">
+            <input
+              type="text"
+              className="form-control"
+              placeholder="Search a movie"
+              value={search}
+              onChange={(e) => setSearch(e.target.value)}
+            />
+            <button className="btn btn-primary">Search</button>
+          </div>
+        </div>
+      </div>
+    </div>
+  );
 }
 
 export default App;
