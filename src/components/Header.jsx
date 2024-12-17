922991ca9c1d85e4b1be04ed926c8cdd5f9c4032
donatimar <donatimar@gmail.com>
2024-12-17 16:40:29 +0100

    feat: add styling to Header

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -5,25 +5,29 @@ function Header() {
   const { search, setSearch, handleSearch, isLoading } = useContext(AppContext);
 
   return (
-    <div>
-      <h1 className="text-center mb-3">Movies & Tv Series</h1>
-      <div className="row justify-content-center mb-3">
-        <div className="col">
-          <div className="input-group">
-            <input
-              type="text"
-              className="form-control"
-              placeholder="Search a movie"
-              value={search}
-              onChange={(e) => setSearch(e.target.value)}
-            />
-            <button
-              className="btn btn-primary"
-              onClick={handleSearch}
-              disabled={isLoading}
-            >
-              {isLoading ? "Searching" : "Search"}
-            </button>
+    <div className="bg-dark py-3">
+      <div className="container">
+        <div className="row align-items-center">
+          <div className="col d-flex">
+            <h1 className="text-danger mb-0">BOOLFLIX</h1>
+          </div>
+          <div className="col-4 d-flex justify-content-end">
+            <div className="input-group">
+              <input
+                type="text"
+                className="form-control"
+                placeholder="Search a movie"
+                value={search}
+                onChange={(e) => setSearch(e.target.value)}
+              />
+              <button
+                className="btn btn-danger"
+                onClick={handleSearch}
+                disabled={isLoading}
+              >
+                {isLoading ? "Searching" : "Search"}
+              </button>
+            </div>
           </div>
         </div>
       </div>
